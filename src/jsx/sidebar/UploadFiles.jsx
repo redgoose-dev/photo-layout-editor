443a82877372f1e20c906d9d1ece7850eaf1180b
redgoose <scripter@me.com>
2016-05-10 08:12:19 +0900

    드래그 이벤트 작업을 위한 준비

--- src/jsx/sidebar/UploadFiles.jsx
+++ src/jsx/sidebar/UploadFiles.jsx
@@ -66,16 +66,36 @@ module.exports = React.createClass({
 	 */
 	onDragStart(e)
 	{
-		this.$gridster.find('li').on('dragover', (e) => {
-			e.preventDefault();
-			$(e.currentTarget).addClass('hover');
-		}).on('dragleave', (e) => {
-			e.preventDefault();
-			$(e.currentTarget).removeClass('hover');
-		}).on('drop', (e) => {
-			e.preventDefault();
-			this.dragTarget = e.currentTarget;
-		});
+		var type = e.type;
+
+		// TODO : 터치 드래그 이벤트 때문에 드래그 컴포넌트를 만들어야할거 같음.
+		// TODO : 드래그 시작하면 타겟을 복제해야함. 그리고 마우스 위치를 따라가게 하기 위해서 move 이벤트를 추가할 필요가 있음.
+		// TODO : 마우스 포인트가 대상 영역에 들어가면 마우스 포인트가 변하게 해야함.
+		// TODO : end 이벤트가 일어났으면 복제한 타겟을 삭제하고 드래그 이벤트를 전부 off시킴
+		// TODO : 대상에 들어갔는지 확인하고 대상에다 놓았으면 gridster 업데이트.
+
+		// switch(e.type)
+		// {
+		// 	//case 'dragstart':
+		// 	case 'touchstart':
+		// 		this.$gridster.find('li').on('dragover', (e) => {
+		// 			e.preventDefault();
+		// 			$(e.currentTarget).addClass('hover');
+		// 		}).on('dragleave', (e) => {
+		// 			e.preventDefault();
+		// 			$(e.currentTarget).removeClass('hover');
+		// 		}).on('drop', (e) => {
+		// 			e.preventDefault();
+		// 			this.dragTarget = e.currentTarget;
+		// 		});
+		// 		break;
+		//
+		// 	//case 'touchstart':
+		// 	case 'dragstart':
+		// 		log(e.currentTarget);
+		//
+		// 		break;
+		// }
 	},
 
 	/**
@@ -107,10 +127,12 @@ module.exports = React.createClass({
 		// make item elements
 		this.props.uploadImages.forEach((item, key) => {
 			items.push(
-				<li key={key} data-key={key} draggable="true"
+				<li key={key} data-key={key} draggable="false"
 					onClick={this.onSelect}
 					onDragStart={this.onDragStart}
-					onDragEnd={this.onDragEnd}>
+					onDragEnd={this.onDragEnd}
+					onTouchStart={this.onDragStart}
+					onTouchEnd={this.onDragEnd}>
 					<span style={item.style} className={(item.on) ? 'on' : ''}>.img</span>
 				</li>
 			);
